4d94dc71fb48a6bbea6ad5ece6148118eb345373
Abhidev <abhidevdileep2005@gmail.com>
2026-07-12 15:58:11 +0530

    Phase 1: project setup complete

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Smart Waste Detection System",
+    page_icon="♻️",
+    layout="wide"
+)
+
+st.title("♻️ Smart Waste Detection System")
+
+st.success("Project setup completed successfully!")
+
+st.write("Welcome to our Computer Vision project.")
